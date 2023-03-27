6b28e7cd3f1669ac9bc5dcc574b04ea0e6e01b31
WenzelSSE <wenzel@sse.uni-hildesheim.de>
2023-03-27 14:06:29 +0200

    switched port for web app to 3002

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=3002, debug=True)
     
 
 
