b12f4672351193eced0a23bf6e531125d5cead79
WenzelSSE <wenzel@sse.uni-hildesheim.de>
2023-07-07 10:48:48 +0200

    new endpoints
    POST /skill-repositories/create
    GET /skill-repositories/{owner}
    GET /skill-repositories/byId/{repositoryId}
    POST /skill-repositories/{repositoryId}/skill/add_skill
    GET /skill-repositories/skill/{skillId}
    POST /skill-repositories/findSkills

--- sse_gateway.py
+++ sse_gateway.py
@@ -14,7 +14,112 @@ api_instance = sseClient.SkillApi(api_client=api_client)
 lu_api_instance = sseClient.LearningUnitApi(api_client=api_client)
 
 
+def getRepositoryByOwner(ownerId):
 
+    
+    
+    try:
+    # Get list of exposure types
+        print(api_instance.api_client.configuration.host)
+        api_response = api_instance.skill_mgmt_controller_get_skill('1')
+        print(str(api_response))
+        d = SkillDto(id=api_response.id, nested_skills=api_response.nested_skills, name = api_response.name, level=api_response.level, description=api_response.description)
+        print(d.nested_skills)
+        response = jsonify(d.to_dict())
+        response.status_code = 200 # or 400 or whatever
+        
+        print(response)
+        return response
+       
+    except ApiException as e:
+       
+        return ("Exception when calling Api-> %s\n" % e)
+
+
+def getRepository(id):
+
+    
+    
+    try:
+    # Get list of exposure types
+        print(api_instance.api_client.configuration.host)
+        api_response = api_instance.skill_mgmt_controller_get_skill('1')
+        print(str(api_response))
+        d = SkillDto(id=api_response.id, nested_skills=api_response.nested_skills, name = api_response.name, level=api_response.level, description=api_response.description)
+        print(d.nested_skills)
+        response = jsonify(d.to_dict())
+        response.status_code = 200 # or 400 or whatever
+        
+        print(response)
+        return response
+       
+    except ApiException as e:
+       
+        return ("Exception when calling Api-> %s\n" % e)
+
+def createRepository(id):
+
+    
+    
+    try:
+    # Get list of exposure types
+        print(api_instance.api_client.configuration.host)
+        api_response = api_instance.skill_mgmt_controller_get_skill('1')
+        print(str(api_response))
+        d = SkillDto(id=api_response.id, nested_skills=api_response.nested_skills, name = api_response.name, level=api_response.level, description=api_response.description)
+        print(d.nested_skills)
+        response = jsonify(d.to_dict())
+        response.status_code = 200 # or 400 or whatever
+        
+        print(response)
+        return response
+       
+    except ApiException as e:
+       
+        return ("Exception when calling Api-> %s\n" % e)
+    
+def createSkill(id):
+
+    
+    
+    try:
+    # Get list of exposure types
+        print(api_instance.api_client.configuration.host)
+        api_response = api_instance.skill_mgmt_controller_get_skill('1')
+        print(str(api_response))
+        d = SkillDto(id=api_response.id, nested_skills=api_response.nested_skills, name = api_response.name, level=api_response.level, description=api_response.description)
+        print(d.nested_skills)
+        response = jsonify(d.to_dict())
+        response.status_code = 200 # or 400 or whatever
+        
+        print(response)
+        return response
+       
+    except ApiException as e:
+       
+        return ("Exception when calling Api-> %s\n" % e)    
+
+
+def findSkill(id):
+
+    
+    
+    try:
+    # Get list of exposure types
+        print(api_instance.api_client.configuration.host)
+        api_response = api_instance.skill_mgmt_controller_get_skill('1')
+        print(str(api_response))
+        d = SkillDto(id=api_response.id, nested_skills=api_response.nested_skills, name = api_response.name, level=api_response.level, description=api_response.description)
+        print(d.nested_skills)
+        response = jsonify(d.to_dict())
+        response.status_code = 200 # or 400 or whatever
+        
+        print(response)
+        return response
+       
+    except ApiException as e:
+       
+        return ("Exception when calling Api-> %s\n" % e)    
 
 def getSkill(id):
 
